a17c461c0dea1c579b59cebf06a2df255d169a96
Patrick Massot <patrickmassot@free.fr>
2018-01-06 22:46:01 +0100

    Defines normed_space and differential

--- normed_vector_space.lean
+++ normed_vector_space.lean
@@ -1,48 +1,53 @@
 import analysis.real
 import analysis.metric_space
+import analysis.limits
+import algebra.module
 
 noncomputable theory
 
+-- This will soon be bultin mathlib
 attribute [simp] abs_zero abs_neg
 
-variables {E : Type*} [vector_space ℝ E]
+def goesto {α β : Type*} [topological_space α] [topological_space β] (f : α → β) (a : α) (b : β) : Prop :=
+filter.tendsto f (nhds a) (nhds b)
 
-structure is_norm (N : E → ℝ) : Prop :=
-  (nonneg : ∀ e : E, 0 ≤ N e)
-  (eq_zero : ∀ e : E, N e = 0 → e = 0)
-  (triangle : ∀ e f : E, N (e + f) ≤ N e + N f)
-  (homo : ∀ l : ℝ, ∀ e : E, N (l • e) = abs(l) * N e)
-
-lemma zero_norm (N : E → ℝ) (H : is_norm N) : N 0 = 0 :=
-by simpa using H.homo 0 0
+local notation f `→_{` a `}` b := goesto f a b
 
 lemma abs1 : abs (1:ℝ) = 1 :=
 abs_of_pos zero_lt_one
 
-instance metric_of_norm {E : Type*} [vector_space ℝ E] {N : E → ℝ} (H : is_norm N) : metric_space E :=
-{ dist := λ x y, N (x - y),
-  dist_self := by  simp [H, zero_norm N],
-  eq_of_dist_eq_zero := assume x y N0, eq_of_sub_eq_zero (H.eq_zero _ N0),
-  dist_comm := assume x y, by simpa [abs1] using (H.homo (-1:ℝ) (x -y)).symm,
-  dist_triangle := assume x y z, by simpa using H.triangle (x-y) (y-z) }
 
-structure norm (E : Type*) [vector_space ℝ E] :=
+variables {E : Type*} [vector_space ℝ E]
+
+structure vector_space_norm (E : Type*) [vector_space ℝ E] :=
   (map : E → ℝ)
   (nonneg : ∀ e : E, 0 ≤ map e)
   (eq_zero : ∀ e : E, map e = 0 → e = 0)
   (triangle : ∀ e f : E, map (e + f) ≤ map e + map f)
   (homo : ∀ l : ℝ, ∀ e : E, map (l • e) = abs(l) * map e)
 
-instance : has_coe_to_fun (norm E) := 
-⟨_, norm.map⟩
+instance : has_coe_to_fun (vector_space_norm E) := 
+⟨_, vector_space_norm.map⟩
 
 @[simp]
-lemma zero_norm' (N : norm E) : N 0 = 0 :=
+lemma zero_norm (N : vector_space_norm E) : N 0 = 0 :=
 by simpa using N.homo 0 0
 
-instance metric_of_norm' {E : Type*} [vector_space ℝ E] {N : norm E} : metric_space E :=
+
+def metric_of_norm {E : Type*} [vector_space ℝ E] (N : vector_space_norm E) : metric_space E :=
 { dist := λ x y, N (x - y),
   dist_self := by simp,
   eq_of_dist_eq_zero := assume x y N0, eq_of_sub_eq_zero (N.eq_zero _ N0),
   dist_comm := assume x y, by simpa [abs1] using (N.homo (-1:ℝ) (x -y)).symm,
   dist_triangle := assume x y z, by simpa using N.triangle (x-y) (y-z) }
+
+class normed_space (type : Type*) extends vector_space ℝ type :=
+(norm : vector_space_norm type)
+
+def norm {F : Type*} [nF : normed_space F] : F → ℝ := normed_space.norm F
+
+instance normed_space.to_metric_space {A : Type*} [An : normed_space A] : metric_space A :=
+metric_of_norm An.norm
+
+def is_differential {E F : Type*} [nE : normed_space E] [nF : normed_space F] (f : E → F) (a : E) (L : E → F) (H: is_linear_map L) : Prop :=
+(λ x, (1/(norm (x - a))) • (f x - f a - (L (x -a)))) →_{a} 0
