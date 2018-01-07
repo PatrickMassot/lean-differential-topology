23fcf9a982312c21293ac66cfd459e0570d9a25d
Patrick Massot <patrickmassot@free.fr>
2018-01-07 22:42:14 +0100

    Some progress towards chain rule

--- normed_vector_space.lean
+++ normed_vector_space.lean
@@ -17,24 +17,24 @@ lemma abs1 : abs (1:ℝ) = 1 :=
 abs_of_pos zero_lt_one
 
 
-variables {E : Type*} [vector_space ℝ E]
+variables {V : Type*} [vector_space ℝ V]
 
-structure vector_space_norm (E : Type*) [vector_space ℝ E] :=
-  (map : E → ℝ)
-  (nonneg : ∀ e : E, 0 ≤ map e)
-  (eq_zero : ∀ e : E, map e = 0 → e = 0)
-  (triangle : ∀ e f : E, map (e + f) ≤ map e + map f)
-  (homo : ∀ l : ℝ, ∀ e : E, map (l • e) = abs(l) * map e)
+structure vector_space_norm (V : Type*) [vector_space ℝ V] :=
+  (map : V → ℝ)
+  (nonneg : ∀ e : V, 0 ≤ map e)
+  (eq_zero : ∀ e : V, map e = 0 → e = 0)
+  (triangle : ∀ e f : V, map (e + f) ≤ map e + map f)
+  (homo : ∀ l : ℝ, ∀ e : V, map (l • e) = abs(l) * map e)
 
-instance : has_coe_to_fun (vector_space_norm E) := 
+instance : has_coe_to_fun (vector_space_norm V) := 
 ⟨_, vector_space_norm.map⟩
 
 @[simp]
-lemma zero_norm (N : vector_space_norm E) : N 0 = 0 :=
+lemma zero_norm (N : vector_space_norm V) : N 0 = 0 :=
 by simpa using N.homo 0 0
 
 
-def metric_of_norm {E : Type*} [vector_space ℝ E] (N : vector_space_norm E) : metric_space E :=
+def metric_of_norm {V : Type*} [vector_space ℝ V] (N : vector_space_norm V) : metric_space V :=
 { dist := λ x y, N (x - y),
   dist_self := by simp,
   eq_of_dist_eq_zero := assume x y N0, eq_of_sub_eq_zero (N.eq_zero _ N0),
@@ -44,10 +44,85 @@ def metric_of_norm {E : Type*} [vector_space ℝ E] (N : vector_space_norm E) : 
 class normed_space (type : Type*) extends vector_space ℝ type :=
 (norm : vector_space_norm type)
 
-def norm {F : Type*} [nF : normed_space F] : F → ℝ := normed_space.norm F
+def norm {E : Type*} [normed_space E] : E → ℝ := normed_space.norm E
+local notation `∥` e `∥` := norm e
 
 instance normed_space.to_metric_space {A : Type*} [An : normed_space A] : metric_space A :=
 metric_of_norm An.norm
 
-def is_differential {E F : Type*} [nE : normed_space E] [nF : normed_space F] (f : E → F) (a : E) (L : E → F) (H: is_linear_map L) : Prop :=
-(λ x, (1/(norm (x - a))) • (f x - f a - (L (x -a)))) →_{a} 0
+
+section continuous_linear_maps
+variables {E : Type*} {F : Type*} {G : Type*} [normed_space E] [normed_space F] [normed_space G]
+
+-- TODO: relate to is_continuous
+def is_continuous_linear_map (L : E → F) := (is_linear_map L) ∧  ∃ M, M > 0 ∧ ∀ x : E, ∥ L x ∥ ≤ M *∥ x ∥ 
+
+-- TODO: Clean up this proof
+lemma comp_continuous_linear_map (L : E → F) (P : F → G) : 
+is_continuous_linear_map L → is_continuous_linear_map P → is_continuous_linear_map (P ∘ L) :=
+begin
+intros HL HP,
+rcases HL with ⟨lin_L , M, Mpos, ineq_L⟩,
+rcases HP with ⟨lin_P , M', M'pos, ineq_P⟩,
+split,
+{ exact is_linear_map.comp lin_P lin_L },
+{ existsi M*M',
+  split,
+  { exact mul_pos Mpos M'pos },
+  { unfold function.comp,
+    intro x,
+    specialize ineq_P (L x),
+    specialize ineq_L x,
+    have fact : M'*∥L x∥ ≤ M * M' * ∥x∥ := -- prepare for PAIN
+      begin 
+      have ineq := mul_le_mul_of_nonneg_left ineq_L (le_of_lt M'pos),
+      rw mul_comm, 
+      rw mul_comm at ineq, 
+      rw ←mul_assoc at ineq,
+      rw mul_comm M,  
+      exact ineq
+      end ,
+    exact le_trans ineq_P fact }
+   }
+end
+end continuous_linear_maps
+
+section differential
+variables {E : Type*} {F : Type*} {G : Type*} [normed_space E] [normed_space F] [normed_space G]
+
+def is_differential  (f : E → F) (a : E) (L : E → F) : Prop :=
+(is_continuous_linear_map L) ∧ (∃ ε : E → F, (∀ h, f (a + h) =  f a + L h + ∥h∥ • ε h) ∧  (ε →_{0} 0))
+
+-- set_option trace.check true
+-- set_option pp.all true
+
+theorem chain_rule (f : E → F) (g : F → G) (a : E) (L : E → F) (P : F → G)
+(D : is_differential f a L) (D' : is_differential g (f a) P) : is_differential (g ∘ f) a (P ∘ L) :=
+begin
+rcases D with ⟨cont_lin_L, ε, TEf, lim_ε⟩,
+rcases D' with ⟨cont_lin_P, η, TEg, lim_η⟩,
+unfold is_differential,
+split,
+{ exact comp_continuous_linear_map L P cont_lin_L cont_lin_P },
+existsi _,
+{ split,
+  { intro h,
+    unfold function.comp,
+    rw TEf h,
+    -- Now I'd like to have a calc proof but I always have weird "unknown identifier" errors in calc
+    have fact1 : g (f a + L h + ∥h∥ • ε h) = g (f a + (L h + ∥h∥ • ε h)) := by {simp, },
+    rw fact1, clear fact1,
+    rw TEg,
+    rcases cont_lin_P with ⟨lin_P , cont_P⟩, -- moving this line before "rw fact1" makes the later fails WTF?
+    have fact2 : P (L h + ∥h∥ • ε h) = P (L h) + P (∥h∥ • ε h) := lin_P.add _ _,
+    have fact3 : P (L h + ∥h∥ • ε h) = P (L h) + ∥h∥ • P (ε h) := by simp[fact2, lin_P.smul],
+    clear fact2,
+    -- The following line doesn't work. No idea why. Going to bed.
+    rw fact3,
+
+    admit },
+  { admit }, },
+{ admit },
+end
+
+end differential
