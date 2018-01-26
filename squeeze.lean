c485411d68783027dcd24c16301c04cdd5ccee8a
Patrick Massot <patrickmassot@free.fr>
2018-01-26 09:43:08 +0100

    Clean squeeze proof

--- squeeze.lean
+++ squeeze.lean
@@ -3,41 +3,51 @@ import ineq
 
 local notation f `→_{`:50 a `}`:0 b := filter.tendsto f (nhds a) (nhds b)
 
+lemma abs_le_max_abs_abs {α : Type*} [decidable_linear_ordered_comm_group α] {a b c : α} :
+a ≤ b → b ≤ c → abs b ≤ max (abs a) (abs c) :=
+begin
+    intros hab hbc,
+    apply abs_le_of_le_of_neg_le,
+    { exact (calc
+        b ≤ c : hbc
+        ... ≤ abs c : le_abs_self _
+        ... ≤ max (abs a) (abs c): le_max_right _ _) },
+    { exact (calc
+        -b ≤ -a : neg_le_neg hab
+        ... ≤ abs a : neg_le_abs_self _
+        ... ≤ max (abs a) (abs c): le_max_left _ _) }
+end
 
 lemma squeeze {X : Type*} [metric_space X] (f g h : X → ℝ) (x₀ : X) (y : ℝ): 
 (∀ x : X, f x ≤ g x) → (∀ x : X, g x ≤ h x) → (f →_{x₀} y) → (h →_{x₀} y) → (g →_{x₀} y) :=
 begin
-intros ineq_fg ineq_gh lim_f lim_h,
-apply  tendsto_nhds_of_metric.2,
-intros ε ε_pos,
-rcases (tendsto_nhds_of_metric.1 lim_f ε ε_pos) with ⟨δ_f, δ_f_pos, ineq_lim_f⟩,
-rcases (tendsto_nhds_of_metric.1 lim_h ε ε_pos) with ⟨δ_h, δ_h_pos, ineq_lim_h⟩,
-existsi (min δ_f δ_h),
-existsi lt_min δ_f_pos δ_h_pos,
-intros x dist_x,
+    intros ineq_fg ineq_gh lim_f lim_h,
+    apply  tendsto_nhds_of_metric.2,
+    intros ε ε_pos,
+    rcases (tendsto_nhds_of_metric.1 lim_f ε ε_pos) with ⟨δ_f, δ_f_pos, ineq_lim_f⟩,
+    rcases (tendsto_nhds_of_metric.1 lim_h ε ε_pos) with ⟨δ_h, δ_h_pos, ineq_lim_h⟩,
+    existsi (min δ_f δ_h),
+    existsi lt_min δ_f_pos δ_h_pos,
+    intros x dist_x,
 
-have := min_le_left δ_f δ_h,
-have dist_x_δ_f : dist x x₀ < δ_f,
-ineq_trans, clear this,
-have abs_f_x := ineq_lim_f dist_x_δ_f, clear dist_x_δ_f,
-rw [show dist (f x) y = abs (f x - y), from rfl] at abs_f_x,
+    have abs_f_x : dist (f x) y < ε := 
+    ineq_lim_f (calc
+        dist x x₀ < min δ_f δ_h : by assumption
+            ... ≤ δ_f :  min_le_left _ _),
 
-have := min_le_right δ_f δ_h,
-have dist_x_δ_h : dist x x₀ < δ_h,
-ineq_trans, clear this,
+    have abs_h_x : dist (h x) y < ε := 
+    ineq_lim_h (calc
+        dist x x₀ < min δ_f δ_h : by assumption
+            ... ≤ δ_h :  min_le_right _ _),
 
-have abs_h_x := ineq_lim_h dist_x_δ_h, clear dist_x_δ_h,
-rw [show dist (h x) y = abs (h x - y), from rfl] at abs_h_x,
+    have ineq_fg_y : f x - y ≤ g x - y := 
+        (sub_le_sub_iff_right y).2 (ineq_fg x),
 
-have sub_f_gt := (abs_lt.1 abs_f_x).left,
-have tmp := (sub_le_sub_iff_right y).2 (ineq_fg x),
-have g_gt : -ε < g x - y
---, ineq_trans,  Does not work :(
- := lt_of_lt_of_le sub_f_gt tmp,
+    have ineq_gh_y : g x - y ≤ h x - y :=
+        (sub_le_sub_iff_right y).2 (ineq_gh x),
 
-have sub_h_lt := (abs_lt.1 abs_h_x).right,
-have tmp2 := (sub_le_sub_iff_right y).2 (ineq_gh x),
-have g_lt : g x - y < ε := lt_of_le_of_lt tmp2 sub_h_lt,
-
-exact abs_lt.2 ⟨g_gt, g_lt⟩
+    exact calc 
+        dist (g x) y = abs (g x - y) : rfl
+        ... ≤ max (abs (f x - y)) (abs (h x - y)) : by { apply abs_le_max_abs_abs; assumption }
+        ... < ε :  by { apply max_lt ; assumption }
 end
